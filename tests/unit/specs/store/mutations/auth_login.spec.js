8a197e45dfbeea64b26881e389183e6e1e06b8da
Satoshi Ohtsuka <sohtsuka1317@gmail.com>
2018-12-09 01:42:06 +0900

    Implement AUTH_LOGIN mutation

---
+++ tests/unit/specs/store/mutations/auth_login.spec.js
@@ -0,0 +1,12 @@
+import mutations from '@/store/mutations'
+
+describe('AUTH_LOGINミューテーション', () => {
+  it('ミューテーションのペイロード値が状態authに設定されること', () => {
+    const state = {}
+    const token = '1234567890abcdef'
+    const userId = 1
+    mutations.AUTH_LOGIN(state, { token, userId })
+    expect(state.auth.token).toEqual(token)
+    expect(state.auth.userId).toEqual(userId)
+  })
+})
