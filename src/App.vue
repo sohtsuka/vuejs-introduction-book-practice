0964f27c535e7b9739c2bf8f2022486975715d1f
Satoshi Ohtsuka <sohtsuka1317@gmail.com>
2018-12-09 20:51:14 +0900

    Implement error handlers

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,9 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <error-boundary>
+      <router-view/>
+    </error-boundary>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ErrorBoundary from './ErrorBoundary.vue'
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
+
+Vue.component(ErrorBoundary.name, ErrorBoundary)
+
+Vue.config.errorHandler = (err, vm, info) => {
+  console.error('errorHandler err: ', err)
+  console.error('errorHandler vm: ', vm)
+  console.error('errorHandler info: ', info)
+}
 
 new Vue({
   router,
