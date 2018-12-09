bf3aaf3855ad71f0e8dc536e2d1a2468748da988
Satoshi Ohtsuka <sohtsuka1317@gmail.com>
2018-12-09 19:47:24 +0900

    Implement backend server mock

--- dev-server.js
+++ dev-server.js
@@ -3,5 +3,25 @@ const bodyParser = require('body-parser')
 module.exports = app => {
   app.use(bodyParser.json())
 
-  // TODO: ここ以降にAPIの実装内容を追加していく
+  const users = {
+    'foo@example.com': {
+      password: '12345678',
+      userId: 1,
+      token: '1234567890abcdef'
+    }
+  }
+
+  app.post('/auth/login', (req, res) => {
+    const { email, password } = req.body
+    const user = users[email]
+    if (user) {
+      if (user.password !== password) {
+        res.status(401).json({ message: 'ログインに失敗しました' })
+      } else {
+        res.json({ userId: user.userId, token: user.token })
+      }
+    } else {
+      res.status(404).json({ message: 'ユーザーが登録されていません' })
+    }
+  })
 }
